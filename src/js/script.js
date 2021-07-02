7c1ba4a02f600513698afee2d6c8aba246bb38f7
Eugene <belopyckich@gmail.com>
2021-07-02 19:08:23 +0300

    second lesson and homework complete

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+var name = "Ivan";
+let number=7;
+const pi= 3.14;
+
+let leftBorderWidth = 200;
+
+//number 
+//string
+//bool
+//null
+//undefined
+let obj = {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+}
