babdc531b8c678b42552bb4f2518650fc009ba9d
Eugene <belopyckich@gmail.com>
2021-07-06 19:40:03 +0300

    footer ready

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,51 @@ $(document).ready(function(){
       nextArrow: '<button type="button" class="slick-next"><img src=icons/right.png></button>',
       responsive: [
         {
-          breakpoint: 992,
+          breakpoint: 767,
           settings: {
             dots: true,
-            arrows: false
+            arrows: false,
           }
         }
       ]
     })
-  });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item)
+    {
+        $(item).each(function(i)
+        {
+            $(this).on('click',function(e)
+            {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // Model
+
+    $('[data-model=consultation').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.model__close').on('click', function(){
+      $('.overlay, #consultation, #order #exit').fadeOut('.slow');
+    });
+    $('.button_buy').each(function(i)
+    {
+      $(this).on('click', function()
+      {
+        $('#order .model__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('.slow');
+      })
+    });
+});
